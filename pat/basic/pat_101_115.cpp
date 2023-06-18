7b53654656a93df9e25528d8a91ff0143f545f4a
lsqyling <1058018384@qq.com>
2023-06-18 13:44:36 +0800

    basic_1105

--- pat/basic/pat_101_115.cpp
+++ pat/basic/pat_101_115.cpp
@@ -82,7 +82,6 @@ void entry()
 
 namespace basic_104 {
 
-
 int calc_sum_of_digits(int n)
 {
     int sum = 0;
@@ -183,6 +182,112 @@ void entry()
 }
 }
 
+namespace basic_105 {
+constexpr int N = 100'005;
+
+struct node
+{
+    int adr, data, next;
+};
+
+node list[N];
+//1->2->3->4->5->6->7->8 k=3 =>3->2->1->6->5->4-7->8
+int reverse_of_k_nodes(int head, int k)
+{
+    if (head == -1 || list[head].next == -1 || k < 2)
+        return head;
+    int tail = head;
+    for (int i = 0; i < k; ++i)
+    {
+        if (tail == -1)
+            return head;
+        tail = list[tail].next;
+    }
+    int pre = -1, cur = head;
+    while (cur != tail)
+    {
+        auto next = list[cur].next;
+
+        list[cur].next = pre;
+        pre = cur;
+        cur = next;
+
+    }
+    list[head].next = reverse_of_k_nodes(tail, k);
+    return pre;
+}
+
+int reverse_of_list(int head)
+{
+    if (head == -1 || list[head].next == -1)
+        return head;
+    int tail = -1;
+    int pre = -1, cur = head;
+    while (cur != tail)
+    {
+        auto next = list[cur].next;
+
+        list[cur].next = pre;
+        pre = cur;
+        cur = next;
+    }
+    return pre;
+}
+
+void entry()
+{
+    int h1, h2, n;
+    scanf("%d%d%d", &h1, &h2, &n);
+    int adr, data, next;
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d%d%d", &adr, &data, &next);
+        list[adr].adr = adr;
+        list[adr].data = data;
+        list[adr].next = next;
+    }
+    int len1 = 0, len2 = 0;
+    int cur = h1;
+    while (cur != -1)
+    {
+        cur = list[cur].next;
+        ++len1;
+    }
+    cur = h2;
+    while (cur != -1)
+    {
+        cur = list[cur].next;
+        ++len2;
+    }
+    if (len1 >= 2 * len2)
+    {
+        std::swap(h1, h2);
+        std::swap(len1, len2);
+    }
+    h1 = reverse_of_list(h1);
+
+    int cur1 = h1, cur2 = h2;
+    while (cur1 != -1)
+    {
+        auto next1 = list[cur1].next;
+        auto next2 = list[cur2].next;
+
+        list[cur1].next = list[next2].next;
+        list[next2].next = cur1;
+
+        cur2 = list[cur1].next;
+        cur1 = next1;
+    }
+    cur = h2;
+    while (list[cur].next != -1)
+    {
+        printf("%05d %d %05d\n", list[cur].adr, list[cur].data, list[cur].next);
+        cur = list[cur].next;
+    }
+    printf("%05d %d -1", list[cur].adr, list[cur].data);
+}
+}
+
 
 
 int main(int argc, char **argv)
@@ -190,6 +295,7 @@ int main(int argc, char **argv)
 //    basic_101::entry();
 //    basic_102::entry();
 //    basic_103::entry();
-    basic_104::entry();
+//    basic_104::entry();
+    basic_105::entry();
     return 0;
 }
