9b1abccf35e6013a4884d206f6815f2ecd248b40
lsqyling <1058018384@qq.com>
2023-06-30 17:56:38 +0800

    start ladder competition

---
+++ ladder/part_1_60.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+
+
+
+
+
+
+namespace L2_005 {
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+int main(int argc, char **argv)
+{
+
+
+    return 0;
+}
--- pat/simple.cpp
+++ pat/simple.cpp
@@ -1,9 +1,18 @@
 
+#include <cstdio>
+
+namespace _4_1_A {
+
+void entry()
+{
+    printf("There's no one to stop you but yourself.");
+}
+}
 
 
 int main(int argc, char **argv)
 {
-
+    _4_1_A::entry();
 
 
     return 0;
